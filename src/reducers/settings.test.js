ad1290a0ec526345dd2bce8b49fa5c5592e576a4
Marcel Guerin <20742004+hungryogre@users.noreply.github.com>
2018-09-09 13:56:07 -0500

    localize testing to individual reducer

--- src/reducers/settings.test.js
+++ src/reducers/settings.test.js
@@ -1,37 +1,37 @@
-import reducers from '../reducers'
+import settings from './settings'
 
 test('Test initial state', () => {
     let state
-    state = reducers(undefined, {})
-    expect(state).toEqual({ settings: { userName: '', password: '', server: 'localhost', options: { encrypt: false } } })
+    state = settings(undefined, {})
+    expect(state).toEqual({ userName: '', password: '', server: 'localhost', options: { encrypt: false } })
 })
 
 test('EDIT_USERNAME should change just the userName', () => {
     let state
-    state = reducers({ settings: { userName: '', password: '', server: 'localhost', options: { encrypt: false } } }, { type: 'EDIT_USERNAME', payload: 'john' })
-    expect(state).toEqual({ settings: { userName: 'john', password: '', server: 'localhost', options: { encrypt: false } } })
+    state = settings({ userName: '', password: '', server: 'localhost', options: { encrypt: false } }, { type: 'EDIT_USERNAME', payload: 'john' })
+    expect(state).toEqual({ userName: 'john', password: '', server: 'localhost', options: { encrypt: false } })
 })
 
 test('EDIT_PASSWORD should change just the password', () => {
     let state
-    state = reducers({ settings: { userName: 'john', password: '', server: 'localhost', options: { encrypt: false } } }, { type: 'EDIT_PASSWORD', payload: 'secretPassword' })
-    expect(state).toEqual({ settings: { userName: 'john', password: 'secretPassword', server: 'localhost', options: { encrypt: false } } })
+    state = settings({ userName: 'john', password: '', server: 'localhost', options: { encrypt: false } }, { type: 'EDIT_PASSWORD', payload: 'secretPassword' })
+    expect(state).toEqual({ userName: 'john', password: 'secretPassword', server: 'localhost', options: { encrypt: false } })
 })
 
 test('EDIT_HOSTNAME should change just the hostname', () => {
     let state
-    state = reducers({ settings: { userName: 'john', password: 'secretPassword', server: 'localhost', options: { encrypt: false } } }, { type: 'EDIT_HOSTNAME', payload: 'my.sqlserver.com' })
-    expect(state).toEqual({ settings: { userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: false } } })
+    state = settings({ userName: 'john', password: 'secretPassword', server: 'localhost', options: { encrypt: false } }, { type: 'EDIT_HOSTNAME', payload: 'my.sqlserver.com' })
+    expect(state).toEqual({ userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: false } })
 })
 
 test('TOGGLE_ENCRYPT should change option from false to true', () => {
     let state
-    state = reducers({ settings: { userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: false } } }, { type: 'TOGGLE_ENCRYPT' })
-    expect(state).toEqual({ settings: { userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: true } } })
+    state = settings({ userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: false } }, { type: 'TOGGLE_ENCRYPT' })
+    expect(state).toEqual({ userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: true } })
 })
 
 test('TOGGLE_ENCRYPT should change option from true to false', () => {
     let state
-    state = reducers({ settings: { userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: true } } }, { type: 'TOGGLE_ENCRYPT' })
-    expect(state).toEqual({ settings: { userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: false } } })
+    state = settings({ userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: true } }, { type: 'TOGGLE_ENCRYPT' })
+    expect(state).toEqual({ userName: 'john', password: 'secretPassword', server: 'my.sqlserver.com', options: { encrypt: false } })
 })
