789af577f1ba2cc9b5575eece40bb6697f5ec61e
Marcel Guerin <20742004+hungryogre@users.noreply.github.com>
2018-09-09 13:31:32 -0500

    add redux devtools

--- src/store.js
+++ src/store.js
@@ -5,4 +5,5 @@ import root from './reducers'
 
 const sagaMiddleware = createSagaMiddleware()
 //export default compose(applyMiddleware(sagaMiddleware))(createStore)(root)
-export default createStore(root)
+export default createStore(root, undefined,
+    typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
