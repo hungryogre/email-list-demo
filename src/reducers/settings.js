0be76f3c7dd38ea4c8bf1c3270e6e5489a2290e5
Marcel Guerin <20742004+hungryogre@users.noreply.github.com>
2018-09-09 13:42:35 -0500

    add toggle encrypt action

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,6 +1,6 @@
 import { createReducer } from './createReducer'
 
-import { EDIT_USERNAME, EDIT_PASSWORD, EDIT_HOSTNAME } from '../actions/settings'
+import { EDIT_USERNAME, EDIT_PASSWORD, EDIT_HOSTNAME, TOGGLE_ENCRYPT } from '../actions/settings'
 
 const initialState = {
     userName: '',
@@ -12,7 +12,8 @@ const initialState = {
 const handlers = {
     [EDIT_USERNAME]: (state, action) => { return { userName: action.payload } },
     [EDIT_PASSWORD]: (state, action) => { return { password: action.payload } },
-    [EDIT_HOSTNAME]: (state, action) => { return { server: action.payload } }
+    [EDIT_HOSTNAME]: (state, action) => { return { server: action.payload } },
+    [TOGGLE_ENCRYPT]: (state, action) => { return { options: { encrypt: !state.options.encrypt } } },
 }
 
 export default createReducer(initialState, handlers)
